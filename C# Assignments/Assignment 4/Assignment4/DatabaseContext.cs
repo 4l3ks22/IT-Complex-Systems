07f8a0577b3e3299b8920551c2b0de3dff4645c5
4l3ks22 <ak.alexanderkuba22@gmail.com>
2025-10-10 13:21:47 +0200

    Alternated Product.cs domain and changed context for both category and product

--- C# Assignments/Assignment 4/Assignment4/DatabaseContext.cs
+++ C# Assignments/Assignment 4/Assignment4/DatabaseContext.cs
@@ -7,6 +7,7 @@ namespace Assignment4;
 public class DatabaseContext : DbContext
 {
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -17,7 +18,20 @@ public class DatabaseContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Context for Category
         modelBuilder.Entity<Category>().ToTable("categories");
         modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+        modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+        modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
+        
+        // Context For Product
+        modelBuilder.Entity<Product>().ToTable("products");
+        modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
+        modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+        modelBuilder.Entity<Product>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+        modelBuilder.Entity<Product>().Property(x => x.QuantityPerUnit).HasColumnName("quantityperunit");
+        modelBuilder.Entity<Product>().Property(x => x.UnitsInStock).HasColumnName("unitsinstock");
+
+        
     }
 }
--- C# Assignments/Assignment 4/Assignment4/Product.cs
+++ C# Assignments/Assignment 4/Assignment4/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public int UnitPrice { get; set; }
-    public int CategoryId { get; set; }
+    public int QuantityPerUnit { get; set; }
+    public int UnitsInStock { get; set; }
     
-    public Category Category { get; set; }
 }
