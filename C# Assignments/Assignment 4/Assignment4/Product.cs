28bdd6244e0470194f73c6536486d9be740a51bd
4l3ks22 <ak.alexanderkuba22@gmail.com>
2025-10-10 13:01:59 +0200

    Added category databasecontext and product

---
+++ C# Assignments/Assignment 4/Assignment4/Product.cs
@@ -0,0 +1,11 @@
+namespace Assignment4;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int UnitPrice { get; set; }
+    public int CategoryId { get; set; }
+    
+    public Category Category { get; set; }
+}
