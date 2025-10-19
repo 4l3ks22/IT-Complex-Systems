6d4f434442205763881de8dba738c9df23f94cc6
Aleks <114920058+4l3ks22@users.noreply.github.com>
2025-10-19 21:14:08 +0200

    Delete

--- C# Assignments/Assignment 4 Part II/DataServiceLayer/Category.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataServiceLayer;
-public class Category
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-}
--- C# Assignments/Assignment 4 Part II/DataServiceLayer/ProductSearchModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DataServiceLayer;
-public class ProductSearchModel
-{
-    public string? ProductName { get; set; }
-    public string? CategoryName { get; set; }
-}
